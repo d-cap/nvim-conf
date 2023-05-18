3927401ea1a08cfb9c88f509051aba9d4197f8e5
Davide Capomagi <davide.capomagi@gdsgroup.com>
2023-05-18 11:05:45 +0100

    Added cursor line and format key binding

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -29,6 +29,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>fmt", function() vim.lsp.buf.format() end, opts)
 end)
 
 local opts = {
